c51a7126d5e893476efa9a187889ec7d308c9367
Vladimir <dr.frisbi@gmail.com>
2022-12-25 15:46:53 +0700

    add 017

---
+++ 017/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите координату X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine($"Точка с координатами {x} и {y} находится в первой четверти");
+}
+else
+{
+    if (x < 0 && y > 0)
+    {
+        Console.WriteLine($"Точка с координатами {x} и {y} находится во второй четверти");
+    }
+    else
+    {
+        if (x < 0 && y < 0)
+        {
+           Console.WriteLine($"Точка с координатами {x} и {y} находится в третьей четверти"); 
+        }
+        else
+        {
+            Console.WriteLine($"Точка с координатами {x} и {y} находится в четвертой четверти");
+        }
+    }
+}
