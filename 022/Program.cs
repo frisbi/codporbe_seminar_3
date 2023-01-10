81312664d4833e99d98502b7ce761b9dfe27209b
Vladimir <dr.frisbi@gmail.com>
2023-01-10 21:13:06 +0700

    add 022

---
+++ 022/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int Index = 1;
+while (Index <= a)
+{
+    Console.WriteLine($"Квадраты чисел от 1 до {a} равняются {Index*Index}");
+    Index+=1;
+}
